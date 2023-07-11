81c829234db53d55d234e5318e602eb240cb126a
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 14:05:15 +0530

    Create A1.java

---
+++ A1.java
@@ -0,0 +1,15 @@
+/* User-defined Package */
+package abhi;
+
+class A 
+{
+    void show()
+    {
+        System.out.println("Learn Coding..!");
+    }
+    public static void main(String[] args) {
+        
+        A r=new A();
+        r.show();
+    }
+}
