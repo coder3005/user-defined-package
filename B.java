94339ddc186c75ba615b384e8886782500bf9cf6
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 14:04:06 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,18 @@
+/* User-defined Package */
+package abhi;
+
+class A 
+{
+    void show()
+    {
+        System.out.println("Learn Coding..!");
+    }
+}
+class B 
+{
+    public static void main(String[] args) {
+        
+        A r=new A();
+        r.show();
+    }
+}
