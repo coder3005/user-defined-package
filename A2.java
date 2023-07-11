35758b415be7a4e25bbf594de6f4b9606c1099ab
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 14:12:29 +0530

    Create A2.java

---
+++ A2.java
@@ -0,0 +1,15 @@
+/* User-defined Package */
+package abhi;  // for running the code, type "javac -d . A.java" and write "java abhi.A " in the next step"
+
+class A 
+{
+    protected void show()
+    {
+        System.out.println("Learn Coding..!");
+    }
+    public static void main(String[] args) {
+        
+        A r=new A();
+        r.show();
+    }
+}
